bd42cc8bd5f7ac610fcd39159ed01fc93c11f71e
MelodiaM5 <melodiamohad@gmail.com>
2021-12-09 13:18:45 +0000

    feat() test add

--- src/main/java/com/cicdlectures/menuserver/controller/MenuController.java
+++ src/main/java/com/cicdlectures/menuserver/controller/MenuController.java
@@ -3,8 +3,10 @@ package com.cicdlectures.menuserver.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -13,6 +15,7 @@ import org.springframework.http.HttpStatus;
 import com.cicdlectures.menuserver.dto.MenuDto;
 import com.cicdlectures.menuserver.service.CreateMenuService;
 import com.cicdlectures.menuserver.service.ListMenuService;
+import com.cicdlectures.menuserver.repository.MenuRepository;
 
 @RestController
 public class MenuController {
@@ -21,10 +24,13 @@ public class MenuController {
 
   private final ListMenuService listMenuService;
 
+  private final MenuRepository menuRepository;
+
   @Autowired
-  MenuController(CreateMenuService createMenuService, ListMenuService listMenuService) {
+  MenuController(CreateMenuService createMenuService, ListMenuService listMenuService, MenuRepository menuRepository) {
     this.createMenuService = createMenuService;
     this.listMenuService = listMenuService;
+    this.menuRepository = menuRepository;
   }
 
   @GetMapping(path = "/menus", produces = "application/json")
@@ -45,7 +51,7 @@ public class MenuController {
 
 
 
-    
+
     
   }
 
